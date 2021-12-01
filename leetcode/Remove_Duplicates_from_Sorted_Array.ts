3aa799ab51d6ec81af309f3d394f71c20b45f6ba
Ahsan Habib Abir <abir4584@gmail.com>
2021-12-02 03:08:00 +0600

    2nd commit

---
+++ leetcode/Remove_Duplicates_from_Sorted_Array.ts
@@ -0,0 +1,19 @@
+function removeDuplicates(nums: number[]): number {
+   
+
+     //HIGHLIGHT TODO
+    
+    for (let i = 0; i < nums.length; i++) {
+
+        let j=i+1
+        if (nums[i] === nums[j]) {
+            
+            nums.splice(j,1)
+            i--
+        }
+    }
+        console.log(nums) 
+    
+    return nums.length; 
+
+};
